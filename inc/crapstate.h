8a671e396c2a9ea35bccb964c23a0f9e6466518c
fufixman <freeman121634@gmail.com>
2025-10-16 02:40:35 +0200

    better tomove it here since it isnt used anywhere except in ppu and state

--- inc/crapstate.h
+++ inc/crapstate.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <stdint.h>
 #include "defines.h"
+#include "ppu.h"
 #include "stdio.h"
 
 #ifdef ENABLE_GRAPHICS
--- inc/ppu.h
+++ inc/ppu.h
@@ -1,6 +1,17 @@
 #pragma once
 
 #include <stdint.h>
+
+//PPU defines
+typedef enum {
+    MODE0_HBLANK        = 0,
+    MODE1_VBLANK        = 1,
+    MODE2_OAM           = 2,
+    MODE3_DRAW          = 3,
+    MODE_FAKE_VBLANK    = 4, //occurs after bootrom and lasts 4 cycles
+    MODE_DISABLED       = 5
+} ppu_mode_t;
+
 void update_ppu (uint16_t clocks);
 void stop_ppu();
 void start_ppu();
